00c0df99e5a91f5ed6493f15dca4aa55f481b910
puri <puripant@gmail.com>
2017-01-22 16:15:28 +0700

    Highlight selected point and its guide along y-axis

--- js/line-tabs.js
+++ js/line-tabs.js
@@ -1,4 +1,4 @@
-/* global ThreesyLine document */
+/* global ThreesyLine document d3 */
 
 const data = [
   { x: 'Oct 2016', y: Math.random() },
@@ -15,12 +15,31 @@ const lineChart = new ThreesyLine({
   classes: ['threesy-line-chart', 'line-chart'],
   data,
 });
-
 const getValue = ThreesyLine.getValue;
 
+const selectPoint = (selectedDatum) => {
+  d3.selectAll('.threesy-data-point')
+      .style('stroke', 'darkgray')
+      .style('fill', 'darkgray')
+    .filter(d => d === selectedDatum)
+      .style('stroke', 'darkred')
+      .style('fill', 'darkred');
+  d3.selectAll('.threesy-grid-line-x')
+      .style('stroke', 'lightgray')
+      .style('stroke-dasharray', '5, 5')
+    .filter(d => d === selectedDatum)
+      .style('stroke', 'darkred')
+      .style('stroke-dasharray', '5, 0');
+};
+
 lineChart.draw();
+selectPoint(data.length - 1);
 
 // Enable tooltips using Tether tooltip.
 lineChart.dataPoints
   .attr('data-tooltip', d => `${getValue(lineChart.accessorX, d)}: ${getValue(lineChart.accessorY, d).toFixed(3)}`)
   .attr('data-tooltip-position', 'top center');
+d3.selectAll('.threesy-data-point')
+  .on('click', (d) => {
+    selectPoint(d);
+  });
